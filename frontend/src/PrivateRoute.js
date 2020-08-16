faa7538c1713394d728f39e8103c21aab272f73b
boris <boris.px1@gmail.com>
2020-08-16 22:47:13 +0200

    Fixed bug on the login transition

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -3,20 +3,25 @@ import { Route, Redirect } from 'react-router-dom';
 import Api from './Api'
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-  const [redirect, setRedirect] = useState(false) 
+  const [redirect, setRedirect] = useState(null) 
   
   useEffect(() => {
     const checkStatus = async () => {
       const api = new Api()
       const isLogin = await api.status()
       if (!isLogin) setRedirect(true)
+      else setRedirect(false)
     }
     checkStatus()
   }, [])
-  return (<Route {...rest} render={props => (
-    !redirect ?
-      <Component {...props} />
-      : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )}
-  />)
+  if (redirect === null)
+    return <div></div>
+  else {
+    return (<Route {...rest} render={props => (
+      !redirect ?
+        <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+      )}
+    />)
+  }
 }
