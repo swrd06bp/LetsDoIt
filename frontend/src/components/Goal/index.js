026d0f5ab168c5990798e46a4dc7214b5ae52cdf
boris <boris.px1@gmail.com>
2020-09-14 18:22:57 +0100

    Fixed bug on frontend

--- frontend/src/components/Goal/index.js
+++ frontend/src/components/Goal/index.js
@@ -19,11 +19,11 @@ function Goal (props) {
         goal: props.goal ? null : props.item,
       })}
       onMouseOver={(event) => {
-        if (!props.project && event.target.className === 'task')
+        if (!props.goal || (props.goal && props.goal._id !== props.item._id))
           event.target.style.background = '#FAFAFA'
       }}
       onMouseLeave={(event) => {
-        if (!props.project && event.target.className === 'task')
+        if (!props.goal || (props.goal && props.goal._id !== props.item._id))
           event.target.style.background = 'white'
       }}
     >
--- frontend/src/components/Project/index.js
+++ frontend/src/components/Project/index.js
@@ -19,11 +19,11 @@ function Project (props) {
         project: props.project ? null : props.item,
       })}
       onMouseOver={(event) => {
-        if (!props.project && event.target.className === 'task')
+        if (!props.project || (props.project && props.project._id !== props.item._id))
           event.target.style.background = '#FAFAFA'
       }}
       onMouseLeave={(event) => {
-        if (!props.project && event.target.className === 'task')
+        if (!props.project || (props.project && props.project._id !== props.item._id))
           event.target.style.background = 'white'
       }}
     >
