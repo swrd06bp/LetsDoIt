459c7fdc4bbf9664970a400292d58f19f9e49f21
boris <boris.px1@gmail.com>
2020-12-16 21:15:56 +0100

    Finished the habit tab for the goal description

--- frontend/src/pages/home/HabitsTab.js
+++ frontend/src/pages/home/HabitsTab.js
@@ -1,14 +1,55 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
+import Modal from 'react-modal'
+
+import HabitProgress from './GoalProgress'
 import ListHabit from '../../components/AddGoal/ListHabit'
 import AddHabit from '../../components/AddGoal/AddHabit' 
+import BehaviorForm from '../../components/AddGoal/BehaviorForm'
+import { getDimRatio } from '../../app/DynamicSizing'
+
 
 function HabitsTab (props) {
-  const [update, forceUpdate] = useReducer(x => x + 1, 0)
+  const [showModal, setShowModal] = useState(false)
+  const [chosenHabit, setChosenHabit] = useState(null)
+  
+  useEffect(() => {
+    Modal.setAppElement('body')
+  }, [])
 
   return (
     <div style={styles().habitsWrapper}>
-      <AddHabit goalId={props.goal._id} getAllHabits={forceUpdate} />
-      <ListHabit goalId={props.goal._id} update={update} />
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        style={styles()}
+        contentLabel="Example Modal"
+      >
+        {showModal && ( 
+          <div style={styles().modalContainer}>
+            <BehaviorForm goalId={props.goal._id}/>
+          </div>
+        )}
+      </Modal>
+      <div
+        style={styles().button}
+        onClick={() => setShowModal(true)}
+        onMouseOver={(event) => {
+          event.target.style.background = '#58FAD0'
+        }}
+        onMouseLeave={(event) => {
+          event.target.style.background = '#32A3BC'
+        }}
+      >
+      Edit habits 
+      </div>
+      <ListHabit
+        isClickable={true}
+        onChooseHabit={setChosenHabit}
+        goalId={props.goal._id} 
+      />
+      {chosenHabit && (
+        <HabitProgress habit={chosenHabit} />
+      )}
     </div>
   )
 }
@@ -18,6 +59,33 @@ const styles = () => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+  },
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    borderRadius: 20,
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  modalContainer: {
+    display: 'flex',
+    width: 1300 * getDimRatio().X,
+    height: 1000 * getDimRatio().Y,
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'lightblue',
+    cursor: 'pointer',
+    borderRadius: 20,
+    color: 'white',
+    textAlign: 'center',
+    width: 100,
+    fontWeight: 'bold',
+    marginBottom: 20 * getDimRatio().Y,
   },
 })
 
