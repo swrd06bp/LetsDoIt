18e08715e59bf9fef39e392c8992fe933b7c24ef
boris <boris.px1@gmail.com>
2021-02-10 22:33:51 +0100

    Show notification

--- frontend/src/components/TaskList/SimpleTask.js
+++ frontend/src/components/TaskList/SimpleTask.js
@@ -69,7 +69,7 @@ function SimpleTask (props) {
         </div>
       </div>
       <div style={{display:'flex', flexDirection: 'row', alignItems: 'center'}}>
-      {props.item.isNotification && props.type === 'day' && ( 
+      {props.item.isNotification && !props.item.doneAt && props.type === 'day' && ( 
         <img 
           src='/notification.png' 
           alt='sdf'
